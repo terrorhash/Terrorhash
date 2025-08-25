fcc74090e5845a7097448263e75418db6ea63fcc
terrorhash <info@terrorhash.com>
2025-08-25 23:54:02 +0200

    Update route.ts

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import events from "@/data/events";   // <— holt die Liste aus app/data/events.ts
+import events from "@/app/data/events";   // <— holt die Liste aus app/data/events.ts
 
 export async function GET() {
   return NextResponse.json(events);
